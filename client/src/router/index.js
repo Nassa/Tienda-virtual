0767e3581e24322dfc8df90cd254da0e2e33c97a
juanp-suarez <jpsuarez40@gmail.com>
2021-10-09 12:10:13 -0500

    commit 1 JP

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
+import CRUDE from '../views/CRUDE.vue'
 
 
 const routes = [
@@ -19,6 +20,12 @@ const routes = [
     path: "/register",
     name:"Register",
     component: Register
+  }, 
+
+  {
+    path: "/crude",
+    name: "CRUDE",
+    component: CRUDE
   }
 ]
 
