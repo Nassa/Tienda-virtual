30b2b68f0c5e1c38d85ef1cedc2578f7c6aac5d4
AdrianaA <adrianaachurydiaz@hotmail.com>
2021-10-09 11:35:42 -0500

    commit inicial

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '61bc740d08b92e5e9365ec86c0a049ea'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '22e8568c-b3a4-4415-ad1e-f034bea30a22'
+};
